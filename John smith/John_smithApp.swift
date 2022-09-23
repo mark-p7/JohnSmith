1fd12b23d2409b6f27f4e113b543156762ebfca1
Mark <markdeguzman1142@gmail.com>
2022-09-23 14:57:10 -0700

    Initialized Project

---
+++ John smith/John_smithApp.swift
@@ -0,0 +1,17 @@
+//
+//  John_smithApp.swift
+//  John smith
+//
+//  Created by Mark De Guzman on 2022-09-23.
+//
+
+import SwiftUI
+
+@main
+struct John_smithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
