f8a3f789224459ab4d1a02776e56532c2b51b7ad
Michael Lin <mlin90@se12l0325-00.ad.bcit.ca>
2022-10-07 16:03:01 -0700

    second first commit

--- John smith/ContentView.swift
+++ John smith/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             Text("Hello, world!")
-            Text("Hello 2")
+            Text("Hello round 2")
         }
     }
 }
